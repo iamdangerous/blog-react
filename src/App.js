ae903b0e7b19a3e73f093c42d75649ff84c7640f
Rahul Lohra <rahul.lohra@engineer.ai>
2018-12-02 18:18:17 +0530

    checlpoint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+            <div>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                  <li><Link to="/about">About</Link></li>
+                  <li><Link to="/contact">Contact</Link></li>
+              </ul>
+                <Switch>
+                    <Route path="/" exact component={Index} />
+                    <Route path="/about" exact component={About} />
+                    <Route path="/contact" exact component={Users} />
+                </Switch>
+
+            </div>
+        </Router>
+
     );
   }
 }
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
 
 export default App;
