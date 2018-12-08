dfe6d85e876aa1720baebf70c32c12083c10c5ba
Rahul Lohra <rahul.lohra@engineer.ai>
2018-12-08 14:43:38 +0530

    add code for development and prod

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,2 +1,3 @@
-const BASE_URL = "http://localhost:3002"
+const BASE_URL = (process.env.NODE_ENV === 'development') ? "http://localhost:3002"
+: "https://blog-backend-r.herokuapp.com/"
 export const GET_POSTS_URL = BASE_URL + "/post"
