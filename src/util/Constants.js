478798e3d82de01b0fe7d1c33adf78f789691e9e
Rahul Lohra <rahul.lohra@engineer.ai>
2018-12-08 15:08:22 +0530

    update base url

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,3 +1,4 @@
 const BASE_URL = (process.env.NODE_ENV === 'development') ? "http://localhost:3002"
-: "https://blog-backend-r.herokuapp.com/"
+: "https://blog-backend-r.herokuapp.com"
+console.log(process.env.NODE_ENV)
 export const GET_POSTS_URL = BASE_URL + "/post"
