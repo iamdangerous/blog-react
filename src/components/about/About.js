461d83151507d1143ed23bf0c191acb6a8d90cec
Rahul Lohra <rahul.lohra@engineer.ai>
2018-12-23 16:42:53 +0530

    update about

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,25 @@
 import React from "react";
+import classes from './About.css';
 
 const About = (props)=>{
+    const spanStyle = {
+        color: '#53f6c7'
+      };
     return (
-        <h1>About</h1>
+        <div className = {classes.About}>
+            
+            <div className = {classes.AboutNameContainer}>
+                <p className = {classes.title}>Hello!</p>
+                <p className = {classes.name}><span style={spanStyle}>I'm Rahul</span> Lohra!</p>
+                <ul>
+                    <li>Android Developer</li>
+                    <li>Ios Developer</li>
+                    <li>Backend Developer</li>
+                </ul>
+            </div>
+            
+        </div>
+        
     )
 };
 
